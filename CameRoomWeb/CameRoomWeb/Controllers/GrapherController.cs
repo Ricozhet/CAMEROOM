e66f2d73291784e89345a78d85e5953c6fefbd1f
peerawatkung <peerawatkung@gmail.com>
2013-04-20 18:36:53 +0700

    Create GrapherModel

---
+++ CameRoomWeb/CameRoomWeb/Controllers/GrapherController.cs
@@ -0,0 +1,26 @@
+﻿using CameRoomWeb.Models.GrapherModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CameRoomWeb.Controllers
+{
+    public class GrapherController : Controller
+    {
+        //
+        // GET: /Grapher/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GrapherRegister()
+        {
+            GrapherRegisterModel model = new GrapherRegisterModel();
+            return View(model);
+        }
+
+    }
+}
